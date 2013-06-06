b25d54624f98726483c25a4f5f25419b1ae42660
Christopher Brown <io@henrian.com>
2013-06-06 07:34:42 -0500

    Trim down defaults out of TimeoutDetector.

--- timeout.js
+++ timeout.js
@@ -3,15 +3,10 @@ var util = require('util');
 var stream = require('stream');
 
 var TimeoutDetector = module.exports = function(opts) {
-  if (opts === undefined) opts = {};
-  // opts = {[timeout: 60 (seconds)]}
+  if (!opts || !opts.timeout) throw new Error('TimeoutDetector({timeout: ...}) is a required parameter.');
   stream.Transform.call(this, opts);
-
-  if (opts.timeout !== undefined) {
-    // ensure we get something every x seconds.
-    this.timeout_ms = opts.timeout * 1000;
-    setInterval(this._check.bind(this), this.timeout_ms);
-  }
+  this.timeout_ms = opts.timeout * 1000;
+  setInterval(this._check.bind(this), this.timeout_ms);
 };
 util.inherits(TimeoutDetector, stream.Transform);
 
@@ -19,7 +14,8 @@ TimeoutDetector.prototype._check = function() {
   // silent_ms: milliseconds since we got some incoming data
   var silent_ms = Date.now() - this.last;
   if (silent_ms > this.timeout_ms) {
-    this.emit('error', new Error('Timeout pipe timed out.'));
+    // ensure we get something every x seconds.
+    this.emit('error', new Error('TimeoutDetector timed out.'));
   }
 };
 
