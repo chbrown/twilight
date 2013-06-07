36b16cad05a0d00c28f9a8918999727951157efd
Christopher Brown <io@henrian.com>
2013-06-07 07:43:11 -0500

    Clean up rtcount a bit

--- bin/rtcount.js
+++ bin/rtcount.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-Object.prototype.toArray = function() {
-  var self = this;
-  return Object.keys(this).map(function(key) {
-    return [key, self[key]];
-  });
-};
+// Usage:
+// just stream newline-separated json tweets through it.
+// Ctrl+C when necessary or will exit when STDIN ends.
+
+function parseJSON(s) {
+  try { return JSON.parse(s); }
+  catch (exc) { return null; }
+}
 
 var buf = '';
 process.stdin.resume();
@@ -14,25 +16,17 @@ process.stdin.on('data', function(chunk) {
   var parts = buf.split(/\n/);
   buf = parts[parts.length];
   for (var i = 0, l = parts.length - 1; i < l; i++) {
-    var obj = parseLine(parts[i]);
+    var obj = parseJSON(parts[i]);
     if (obj && obj.text)
       processTweet(obj);
   }
 }).on('end', end);
 
-function parseLine(line, callback) {
-  try {
-    return JSON.parse(line);
-  }
-  catch (exc) {
-    return null;
-  }
-}
 
 var retweets = {};
 function processTweet(tweet) {
   if (tweet.text.match(/RT/)) {
-    var text = tweet.text.replace(/\n/g, ' ');
+    var text = tweet.text.replace(/\s+/g, ' '); // brutal
     if (retweets[text] === undefined) {
       retweets[text] = [];
     }
@@ -42,12 +36,17 @@ function processTweet(tweet) {
 }
 
 function end() {
-  retweets.toArray().sort(function(a, b) {
-    return a[1].length - b[1].length;
-  }).forEach(function(item) {
-    var text = item[0], dates = item[1];
-    if (dates.length > 1)
-      console.log(dates.length + '\t' + text + '\t' + dates);
+  var retweets_array = [];
+  for (var text in retweets) {
+    retweets_array.push({text: text, dates: retweets[text]});
+  }
+
+  retweets_array.sort(function(a, b) {
+    return a.dates.length - b.dates.length;
+  }).forEach(function(retweet) {
+    if (retweet.dates.length > 1) {
+      console.log(retweet.dates.length + '\t' + retweet.text + '\t' + retweet.dates.join(','));
+    }
   });
 
   process.exit();
