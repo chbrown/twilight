8e1972cfec1f612e61c9f1b77e8849a26a4a7677
Christopher Brown <io@henrian.com>
2013-06-15 11:03:51 -0500

    Use : to denote buckets, / for subkeys

--- twilight/aggregate.py
+++ twilight/aggregate.py
@@ -11,23 +11,23 @@ r = None
 
 ''' Database:
 
-    twitteragg/read                     set of filenames we've read
-    twitteragg/failed                   set of filenames we made errors in
+    twitteragg:read                     set of filenames we've read
+    twitteragg:failed                   set of filenames we made errors in
 
-    twitteragg/{name}                   int, total count for this name
-    twitteragg/{name}/rt                int, total RT's captured
-    twitteragg/{name}/lang              hash(lang -> int) total tweets in "lang"
+    twitteragg:{name}                   int, total count for this name
+    twitteragg:{name}/rt                int, total RT's captured
+    twitteragg:{name}/lang              hash(lang -> int) total tweets in "lang"
 
-    twitteragg/{name}/{datehour}        int, total count for that hour
-    twitteragg/{name}/{datehour}/rt     int, RT's in that hour
-    twitteragg/{name}/{datehour}/lang   hash(lang -> int) tweets in "lang" for that hour
+    twitteragg:{name}:{datehour}        int, total count for that hour
+    twitteragg:{name}:{datehour}/rt     int, RT's in that hour
+    twitteragg:{name}:{datehour}/lang   hash(lang -> int) tweets in "lang" for that hour
 
 '''
 
 
 def readLine(line, named_prefix):
     tweet = TTV2.from_tsv(line.strip())
-    dated_prefix = named_prefix + '/' + tweet.get('created_at')[:11]
+    dated_prefix = named_prefix + ':' + tweet.get('created_at')[:11]
 
     for prefix in [named_prefix, dated_prefix]:
         r.incr(prefix)
@@ -38,7 +38,7 @@ def readLine(line, named_prefix):
 
 def readFile(filepath):
     filename = os.path.basename(filepath)
-    prefix = 'twitteragg/' + filename.split('_')[0]
+    prefix = 'twitteragg:' + filename.split('_')[0]
 
     stderr('         ' + filename)
     nrows = -1
@@ -68,8 +68,8 @@ def main():
 
     global r
     r = redis.StrictRedis(host=opts.host, socket_timeout=1)
-    keys = len(r.keys('twitteragg/*'))
-    stderrn('Established Redis connection. {keys} twitteragg/* keys.'.format(keys=keys))
+    keys = len(r.keys('twitteragg:*'))
+    stderrn('Established Redis connection. {keys} twitteragg:* keys.'.format(keys=keys))
 
     for ttv2_filepath in glob(opts.files):
         ttv2_filename = os.path.basename(ttv2_filepath)
@@ -79,7 +79,7 @@ def main():
         # (integer) 1
         # SADD myset "one.txt"
         # (integer) 0
-        novel = r.sadd('twitteragg/read', ttv2_filename)
+        novel = r.sadd('twitteragg:read', ttv2_filename)
         if novel:
             try:
                 readFile(ttv2_filepath)
@@ -90,6 +90,6 @@ def main():
             except Exception, exc:
                 stderrn()
                 stderrn(ttv2_filename + ' failed: ' + str(exc))
-                r.sadd('twitteragg/failed', ttv2_filename)
+                r.sadd('twitteragg:failed', ttv2_filename)
         else:
             stderrn(ttv2_filename + ' already read')
