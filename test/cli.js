726ca3d1d34d3e275b8d12a6310e913d20f51faa
Christopher Brown <io@henrian.com>
2014-01-17 21:45:54 -0600

    Standardize tap import

--- test/cli.js
+++ test/cli.js
@@ -1,10 +1,10 @@
 'use strict'; /*jslint node: true, es5: true, indent: 2 */
-var test = require('tap').test;
+var tap = require('tap');
 
 var twitter_curl = require('../bin/twitter-curl').curl;
 var twitter_rest = require('../bin/twitter-rest').rest;
 
-test('import', function(t) {
+tap.test('import', function(t) {
   t.ok(twitter_curl, 'curl should load from ../bin/twitter-curl');
   t.ok(twitter_rest, 'rest should load from ../bin/twitter-rest');
   t.end();
